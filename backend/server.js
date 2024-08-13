3b502c83a0a56f49bdca35eb293e7a695d7d013d
Shashank <shashank.sawarn@ril.com>
2024-08-13 22:23:06 +0530

    Chat Application Project completed

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 
 import connectToMongoDB from './db/connectToMongoDB.js';
+import {app, server} from './socket/socket.js'
 
-const app =express();
 const PORT = 5000;
 
 dotenv.config();
@@ -23,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, ()=> {
+server.listen(PORT, ()=> {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`)
 });
